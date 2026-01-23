20bca0a77b452d4b11771e181f2c9e03785f3781
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-01-23 23:18:15 +0500

    Update template directory path in settings.py

--- b_shop/settings.py
+++ b_shop/settings.py
@@ -44,7 +44,8 @@ ROOT_URLCONF = 'b_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+       # 'DIRS': [BASE_DIR / 'b_shop' / 'templates'],
+        'DIRS': [BASE_DIR / 'b_shop' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
